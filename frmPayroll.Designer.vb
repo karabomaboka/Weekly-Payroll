e5c8b971c70fa12d3bb290dae2f4d4475927e48e
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-01-14 22:24:39 +0200

    Add files via upload

---
+++ frmPayroll.Designer.vb
@@ -0,0 +1,265 @@
+﻿Partial Public Class frmPayroll
+  Inherits System.Windows.Forms.Form
+
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Public Sub New()
+    MyBase.New()
+
+    'This call is required by the Windows Form Designer.
+    InitializeComponent()
+
+  End Sub
+
+  'Form overrides dispose to clean up the component list.
+  <System.Diagnostics.DebuggerNonUserCode()> _
+  Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+    If disposing AndAlso components IsNot Nothing Then
+      components.Dispose()
+    End If
+    MyBase.Dispose(disposing)
+  End Sub
+  Friend WithEvents btnDisplay As System.Windows.Forms.Button
+  Friend WithEvents btnNext As System.Windows.Forms.Button
+  Friend WithEvents btnQuit As System.Windows.Forms.Button
+  Friend WithEvents lblName As System.Windows.Forms.Label
+  Friend WithEvents lblWage As System.Windows.Forms.Label
+  Friend WithEvents lblHours As System.Windows.Forms.Label
+  Friend WithEvents lblAllowancess As System.Windows.Forms.Label
+  Friend WithEvents lblPriorPay As System.Windows.Forms.Label
+  Friend WithEvents lstResults As System.Windows.Forms.ListBox
+
+  'Required by the Windows Form Designer
+
+  'NOTE: The following procedure is required by the Windows Form Designer
+  'It can be modified using the Windows Form Designer.  
+  'Do not modify it using the code editor.
+  <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+    Me.lblAllowancess = New System.Windows.Forms.Label()
+    Me.lblPriorPay = New System.Windows.Forms.Label()
+    Me.lblHours = New System.Windows.Forms.Label()
+    Me.lstResults = New System.Windows.Forms.ListBox()
+    Me.btnDisplay = New System.Windows.Forms.Button()
+    Me.lblName = New System.Windows.Forms.Label()
+    Me.lblWage = New System.Windows.Forms.Label()
+    Me.btnQuit = New System.Windows.Forms.Button()
+    Me.btnNext = New System.Windows.Forms.Button()
+    Me.txtAllowances = New System.Windows.Forms.TextBox()
+    Me.txtName = New System.Windows.Forms.TextBox()
+    Me.txtWage = New System.Windows.Forms.TextBox()
+    Me.txtHours = New System.Windows.Forms.TextBox()
+    Me.txtPriorPay = New System.Windows.Forms.TextBox()
+    Me.grpMarital = New System.Windows.Forms.GroupBox()
+    Me.radMarried = New System.Windows.Forms.RadioButton()
+    Me.radSingle = New System.Windows.Forms.RadioButton()
+    Me.grpMarital.SuspendLayout()
+    Me.SuspendLayout()
+    '
+    'lblAllowancess
+    '
+    Me.lblAllowancess.Location = New System.Drawing.Point(40, 115)
+    Me.lblAllowancess.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblAllowancess.Name = "lblAllowancess"
+    Me.lblAllowancess.Size = New System.Drawing.Size(152, 36)
+    Me.lblAllowancess.TabIndex = 6
+    Me.lblAllowancess.Text = "Number of withholding allowances:"
+    Me.lblAllowancess.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+    '
+    'lblPriorPay
+    '
+    Me.lblPriorPay.AutoSize = True
+    Me.lblPriorPay.Location = New System.Drawing.Point(8, 161)
+    Me.lblPriorPay.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblPriorPay.Name = "lblPriorPay"
+    Me.lblPriorPay.Size = New System.Drawing.Size(182, 17)
+    Me.lblPriorPay.TabIndex = 10
+    Me.lblPriorPay.Text = "Total pay prior to this week:"
+    Me.lblPriorPay.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+    '
+    'lblHours
+    '
+    Me.lblHours.AutoSize = True
+    Me.lblHours.Location = New System.Drawing.Point(23, 89)
+    Me.lblHours.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblHours.Name = "lblHours"
+    Me.lblHours.Size = New System.Drawing.Size(167, 17)
+    Me.lblHours.TabIndex = 4
+    Me.lblHours.Text = "Number of hours worked:"
+    Me.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+    '
+    'lstResults
+    '
+    Me.lstResults.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+    Me.lstResults.FormattingEnabled = True
+    Me.lstResults.ItemHeight = 17
+    Me.lstResults.Location = New System.Drawing.Point(307, 15)
+    Me.lstResults.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lstResults.Name = "lstResults"
+    Me.lstResults.Size = New System.Drawing.Size(355, 208)
+    Me.lstResults.TabIndex = 15
+    '
+    'btnDisplay
+    '
+    Me.btnDisplay.Location = New System.Drawing.Point(19, 244)
+    Me.btnDisplay.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnDisplay.Name = "btnDisplay"
+    Me.btnDisplay.Size = New System.Drawing.Size(176, 39)
+    Me.btnDisplay.TabIndex = 12
+    Me.btnDisplay.Text = "Display Payroll"
+    '
+    'lblName
+    '
+    Me.lblName.AutoSize = True
+    Me.lblName.Location = New System.Drawing.Point(80, 18)
+    Me.lblName.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblName.Name = "lblName"
+    Me.lblName.Size = New System.Drawing.Size(113, 17)
+    Me.lblName.TabIndex = 0
+    Me.lblName.Text = "Employee name:"
+    Me.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+    '
+    'lblWage
+    '
+    Me.lblWage.AutoSize = True
+    Me.lblWage.Location = New System.Drawing.Point(101, 53)
+    Me.lblWage.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.lblWage.Name = "lblWage"
+    Me.lblWage.Size = New System.Drawing.Size(90, 17)
+    Me.lblWage.TabIndex = 2
+    Me.lblWage.Text = "Hourly wage:"
+    Me.lblWage.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+    '
+    'btnQuit
+    '
+    Me.btnQuit.Location = New System.Drawing.Point(417, 242)
+    Me.btnQuit.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnQuit.Name = "btnQuit"
+    Me.btnQuit.Size = New System.Drawing.Size(176, 39)
+    Me.btnQuit.TabIndex = 14
+    Me.btnQuit.Text = "Quit"
+    '
+    'btnNext
+    '
+    Me.btnNext.Location = New System.Drawing.Point(218, 244)
+    Me.btnNext.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.btnNext.Name = "btnNext"
+    Me.btnNext.Size = New System.Drawing.Size(176, 39)
+    Me.btnNext.TabIndex = 13
+    Me.btnNext.Text = "Next Employee"
+    '
+    'txtAllowances
+    '
+    Me.txtAllowances.Location = New System.Drawing.Point(197, 122)
+    Me.txtAllowances.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtAllowances.Name = "txtAllowances"
+    Me.txtAllowances.Size = New System.Drawing.Size(80, 22)
+    Me.txtAllowances.TabIndex = 3
+    '
+    'txtName
+    '
+    Me.txtName.Location = New System.Drawing.Point(197, 15)
+    Me.txtName.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtName.Name = "txtName"
+    Me.txtName.Size = New System.Drawing.Size(80, 22)
+    Me.txtName.TabIndex = 0
+    '
+    'txtWage
+    '
+    Me.txtWage.Location = New System.Drawing.Point(197, 50)
+    Me.txtWage.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtWage.Name = "txtWage"
+    Me.txtWage.Size = New System.Drawing.Size(80, 22)
+    Me.txtWage.TabIndex = 1
+    '
+    'txtHours
+    '
+    Me.txtHours.Location = New System.Drawing.Point(197, 86)
+    Me.txtHours.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtHours.Name = "txtHours"
+    Me.txtHours.Size = New System.Drawing.Size(80, 22)
+    Me.txtHours.TabIndex = 2
+    '
+    'txtPriorPay
+    '
+    Me.txtPriorPay.Location = New System.Drawing.Point(197, 158)
+    Me.txtPriorPay.Margin = New System.Windows.Forms.Padding(4)
+    Me.txtPriorPay.Name = "txtPriorPay"
+    Me.txtPriorPay.Size = New System.Drawing.Size(80, 22)
+    Me.txtPriorPay.TabIndex = 4
+    '
+    'grpMarital
+    '
+    Me.grpMarital.Controls.Add(Me.radMarried)
+    Me.grpMarital.Controls.Add(Me.radSingle)
+    Me.grpMarital.Location = New System.Drawing.Point(84, 192)
+    Me.grpMarital.Margin = New System.Windows.Forms.Padding(4)
+    Me.grpMarital.Name = "grpMarital"
+    Me.grpMarital.Padding = New System.Windows.Forms.Padding(4)
+    Me.grpMarital.Size = New System.Drawing.Size(195, 46)
+    Me.grpMarital.TabIndex = 5
+    Me.grpMarital.TabStop = False
+    Me.grpMarital.Text = "Marital Status"
+    '
+    'radMarried
+    '
+    Me.radMarried.AutoSize = True
+    Me.radMarried.Location = New System.Drawing.Point(107, 20)
+    Me.radMarried.Margin = New System.Windows.Forms.Padding(4)
+    Me.radMarried.Name = "radMarried"
+    Me.radMarried.Size = New System.Drawing.Size(77, 21)
+    Me.radMarried.TabIndex = 1
+    Me.radMarried.Text = "Married"
+    Me.radMarried.UseVisualStyleBackColor = True
+    '
+    'radSingle
+    '
+    Me.radSingle.AutoSize = True
+    Me.radSingle.Location = New System.Drawing.Point(13, 20)
+    Me.radSingle.Margin = New System.Windows.Forms.Padding(4)
+    Me.radSingle.Name = "radSingle"
+    Me.radSingle.Size = New System.Drawing.Size(68, 21)
+    Me.radSingle.TabIndex = 0
+    Me.radSingle.Text = "Single"
+    Me.radSingle.UseVisualStyleBackColor = True
+    '
+    'frmPayroll
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+    Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(676, 294)
+    Me.Controls.Add(Me.grpMarital)
+    Me.Controls.Add(Me.txtPriorPay)
+    Me.Controls.Add(Me.txtHours)
+    Me.Controls.Add(Me.txtWage)
+    Me.Controls.Add(Me.txtName)
+    Me.Controls.Add(Me.txtAllowances)
+    Me.Controls.Add(Me.lstResults)
+    Me.Controls.Add(Me.lblPriorPay)
+    Me.Controls.Add(Me.lblAllowancess)
+    Me.Controls.Add(Me.lblHours)
+    Me.Controls.Add(Me.lblWage)
+    Me.Controls.Add(Me.lblName)
+    Me.Controls.Add(Me.btnQuit)
+    Me.Controls.Add(Me.btnNext)
+    Me.Controls.Add(Me.btnDisplay)
+    Me.Location = New System.Drawing.Point(13, 13)
+    Me.Margin = New System.Windows.Forms.Padding(5, 4, 5, 4)
+    Me.Name = "frmPayroll"
+    Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+    Me.Text = "Weekly Payroll"
+    Me.grpMarital.ResumeLayout(False)
+    Me.grpMarital.PerformLayout()
+    Me.ResumeLayout(False)
+    Me.PerformLayout()
+
+  End Sub
+  Friend WithEvents txtAllowances As System.Windows.Forms.TextBox
+  Friend WithEvents txtName As System.Windows.Forms.TextBox
+  Friend WithEvents txtWage As System.Windows.Forms.TextBox
+  Friend WithEvents txtHours As System.Windows.Forms.TextBox
+  Friend WithEvents txtPriorPay As System.Windows.Forms.TextBox
+  Private components As System.ComponentModel.IContainer
+  Friend WithEvents grpMarital As System.Windows.Forms.GroupBox
+  Friend WithEvents radMarried As System.Windows.Forms.RadioButton
+  Friend WithEvents radSingle As System.Windows.Forms.RadioButton
+
+End Class
---
+++ frmPayroll.vb
@@ -0,0 +1,206 @@
+﻿Public Class frmPayroll
+
+  Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+    Dim empName As String = ""   'Name of employee
+    Dim hrWage As Decimal        'Hourly wage
+    Dim hrsWorked As Decimal     'Hours worked this week
+    Dim allowances As Integer    'Number of withholding allowances for employee
+    Dim prevPay As Decimal       'Total pay for year excluding this week
+    Dim mStatus As String = ""   'Marital status: S for Single; M for Married
+    Dim pay As Decimal           'This week's pay before taxes
+    Dim totalPay As Decimal      'Total pay for year including this week
+    Dim ficaTax As Decimal       'FICA tax for this week
+    Dim fedTax As Decimal        'Federal income tax withheld this week
+    Dim check As Decimal         'Paycheck this week (take-home pay)
+    'Verify and obtain data, compute payroll, display results
+    If Not DataOK() Then
+      Dim msg As String = "At least one piece of requested data is missing" &
+                          " or is provided improperly."
+      MessageBox.Show(msg)
+    Else
+      InputData(empName, hrWage, hrsWorked, allowances, prevPay, mStatus) 'Task 0
+      pay = Gross_Pay(hrWage, hrsWorked)                                  'Task 1
+      totalPay = Total_Pay(prevPay, pay)                                  'Task 2
+      ficaTax = FICA_Tax(pay, prevPay, totalPay, mStatus)                          'Task 3
+      fedTax = Fed_Tax(pay, allowances, mStatus)                          'Task 4
+      check = Net_Check(pay, ficaTax, fedTax)                             'Task 5
+      ShowPayroll(empName, pay, totalPay, ficaTax, fedTax, check)         'Task 6
+    End If
+  End Sub
+
+  Private Sub btnNext_Click(sender As System.Object, e As System.EventArgs) Handles btnNext.Click
+    'Clear all text boxes and radio buttons for next employee's data
+    txtName.Clear()
+    txtWage.Clear()
+    txtHours.Clear()
+    txtAllowances.Clear()
+    txtPriorPay.Clear()
+    radSingle.Checked = False
+    radMarried.Checked = False
+    lstResults.Items.Clear()
+    txtName.Focus()
+  End Sub
+
+  Private Sub btnQuit_Click(sender As System.Object, e As System.EventArgs) Handles btnQuit.Click
+    Me.Close()
+  End Sub
+
+  Function DataOK() As Boolean
+    'Task 0: Validate data
+    If (txtName.Text = "") Or (Not IsNumeric(txtWage.Text)) Or
+       (Not IsNumeric(txtHours.Text)) Or (Not IsNumeric(txtAllowances.Text)) Or
+       (Not IsNumeric(txtPriorPay.Text)) Or
+       ((Not radSingle.Checked) And (Not radMarried.Checked)) Then
+      Return False
+    Else
+      Return True
+    End If
+  End Function
+
+  Sub InputData(ByRef empName As String, ByRef hrWage As Decimal,
+                ByRef hrsWorked As Decimal, ByRef allowances As Integer,
+                ByRef prevPay As Decimal, ByRef mStatus As String)
+    'Task 0: Validate data
+    empName = txtName.Text
+    hrWage = CDec(txtWage.Text)
+    hrsWorked = CDec(txtHours.Text)
+    allowances = CInt(txtAllowances.Text)
+    prevPay = CDec(txtPriorPay.Text)
+    If radMarried.Checked Then
+      mStatus = "M"
+    Else
+      mStatus = "S"
+    End If
+  End Sub
+
+  Function Gross_Pay(hrWage As Decimal, hrsWorked As Decimal) As Decimal
+    'Task 1: Compute weekly pay before taxes
+    If hrsWorked <= 40 Then
+      Return hrsWorked * hrWage
+    Else
+      Return 40 * hrWage + (hrsWorked - 40) * 1.5D * hrWage
+    End If
+  End Function
+
+  Function Total_Pay(prevPay As Decimal, pay As Decimal) As Decimal
+    'Task 2: Compute total pay before taxes
+    Return prevPay + pay
+  End Function
+
+  Function FICA_Tax(pay As Decimal, prevPay As Decimal, totalPay As Decimal,
+                  mStatus As String) As Decimal
+    'Task 3: Compute social security and medicare tax
+    'Calculate social security benefits tax and medicare tax
+    'for a single employee.
+    Const WAGE_BASE As Decimal = 128400D  'There is no Social Security Benefits
+    '                                      tax on income above this level.
+    Const SOCIAL_SECURITY_RATE As Decimal = 0.062D
+    Const MEDICARE_RATE As Decimal = 0.0145D
+    Dim medicareIncreaseBase As Decimal
+    If mStatus = "S" Then
+      medicareIncreaseBase = 200000
+    Else
+      medicareIncreaseBase = 250000
+    End If
+    Dim socialSecurityBenTax, medicareTax, ficaTax As Decimal
+    'Calculate the Social Security Benefits tax.
+    If totalPay <= WAGE_BASE Then
+      socialSecurityBenTax = SOCIAL_SECURITY_RATE * pay
+    ElseIf prevPay < WAGE_BASE Then
+      socialSecurityBenTax = SOCIAL_SECURITY_RATE * (WAGE_BASE - prevPay)
+    End If
+    'Calculate the FICA tax.
+    medicareTax = MEDICARE_RATE * pay
+    If prevPay >= medicareIncreaseBase Then
+      medicareTax += 0.009D * (totalPay - 200000D)
+    End If
+    ficaTax = socialSecurityBenTax + medicareTax
+    Return Math.Round(ficaTax, 2)
+
+  End Function
+
+  Function Fed_Tax(pay As Decimal, allowances As Integer, mStatus As String) As Decimal
+    'Task 4.1: Compute federal income tax withheld rounded to 2 decimal places 
+    Dim adjPay As Decimal
+    Dim tax As Decimal           'unrounded federal tax withheld
+    Const WITHHOLDING_EXEMPTION As Decimal = 79.8D
+    adjPay = pay - (WITHHOLDING_EXEMPTION * allowances)
+    If adjPay < 0 Then
+      adjPay = 0
+    End If
+    If mStatus = "S" Then
+      tax = TaxSingle(adjPay)   'Task 4.2
+    Else
+      tax = TaxMarried(adjPay)  'Task 4.3
+    End If
+    Return Math.Round(tax, 2)   'round to nearest cent
+  End Function
+
+  Function TaxSingle(adjPay As Decimal) As Decimal
+    'Task 4.2: Compute federal tax withheld for single person
+    Select Case adjPay
+      Case 0 To 71
+        Return 0
+      Case 71 To 254
+        Return (0.1D * (adjPay - 71))
+      Case 254 To 815
+        Return 18.3D + 0.12D * (adjPay - 254)
+      Case 815 To 1658
+        Return 85.96D + 0.22D * (adjPay - 815)
+      Case 1658 To 3100
+        Return 271.08D + 0.24D * (adjPay - 1658)
+      Case 3100 To 3917
+        Return 617.16D + 0.32D * (adjPay - 3100)
+      Case 3917 To 9687
+        Return 878.6D + 0.35D * (adjPay - 3917)
+      Case Is > 9687
+        Return 2898.1D + 0.37D * (adjPay - 9687)
+    End Select
+  End Function
+
+  Function TaxMarried(adjPay As Decimal) As Decimal
+    'Task 4.3: Compute federal tax withheld for married person
+    Select Case adjPay
+      Case 0 To 222
+        Return 0
+      Case 222 To 588
+        Return 0.1D * (adjPay - 222)
+      Case 588 To 1711
+        Return 36.6D + 0.12D * (adjPay - 588)
+      Case 1711 To 3395
+        Return 171.36D + 0.22D * (adjPay - 1711)
+      Case 3395 To 6280
+        Return 541.84D + 0.24D * (adjPay - 3395)
+      Case 6280 To 7914
+        Return 1234.24D + 0.32D * (adjPay - 6280)
+      Case 7914 To 11761
+        Return 1757.12D + 0.35D * (adjPay - 7914)
+      Case Is > 11761
+        Return 3103.57D + 0.37D * (adjPay - 11761)
+    End Select
+  End Function
+
+  Function Net_Check(pay As Decimal, ficaTax As Decimal, fedTax As Decimal) As Decimal
+    'Task 5: Compute amount of money paid to employee
+    Return pay - ficaTax - fedTax
+  End Function
+
+  Sub ShowPayroll(empName As String, pay As Decimal,
+                  totalPay As Decimal, ficaTax As Decimal,
+                  fedTax As Decimal, check As Decimal)
+    'Task 6: Display results of payroll computations
+    lstResults.Items.Clear()
+    lstResults.Items.Add("Payroll results for " & empName)
+    lstResults.Items.Add("")
+    lstResults.Items.Add("Gross pay this period:" & "   " & pay.ToString("C"))
+    lstResults.Items.Add("")
+    lstResults.Items.Add("Year-to-date earnings:" & "   " & totalPay.ToString("C"))
+    lstResults.Items.Add("")
+    lstResults.Items.Add("FICA tax this period:" & "    " & ficaTax.ToString("C"))
+    lstResults.Items.Add("")
+    lstResults.Items.Add("Income tax withheld:" & "     " & fedTax.ToString("C"))
+    lstResults.Items.Add("")
+    lstResults.Items.Add("Net pay (check amount):" & "  " & check.ToString("C"))
+  End Sub
+
+End Class
